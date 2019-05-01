1643c0a7d332ed61e3afbbed235189d6d34b715b
someone <someone@someplace.com>
2019-05-01 15:33:09 +0530

    Added profile listing and creating feature.

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import include
 
 from rest_framework.routers import DefaultRouter
 
-
 from . import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
